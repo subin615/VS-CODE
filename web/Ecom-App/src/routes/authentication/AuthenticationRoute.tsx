306527b25f104ca34a4b030b854f14a305365e0b
TY-Vishnu-MowdgalayaBM <vishnu.m@testyantra.in>
2022-02-03 14:56:40 +0530

    redux

--- web/Ecom-App/src/routes/authentication/AuthenticationRoute.tsx
+++ web/Ecom-App/src/routes/authentication/AuthenticationRoute.tsx
@@ -4,29 +4,31 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import NavBar from "../../components/navigationBar/NavBar";
 import Home from "../../pages/home/Home";
 import ProductDetails from "../../pages/productDetails/ProductDetails";
+import User from "../../pages/user/User";
 
 const Registration = lazy(
-  () => import("../../pages/registration/Registration")
+    () => import("../../pages/registration/Registration")
 );
 const Login = lazy(() => import("../../pages/login/Login"));
 
 const AuthenicationRoute: React.FC = () => {
-  const routePath = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [routePath]);
-  return (
-    <>
-      <NavBar />
-      <Routes>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/registration" element={<Registration />}></Route>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/productDetails/:id" element={<ProductDetails />} />
-      </Routes>
-    </>
-  );
+    const routePath = useLocation();
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [routePath]);
+    return (
+        <>
+            <NavBar />
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/registration" element={<Registration />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/productDetails/:id" element={<ProductDetails />} />
+                <Route path="/user" element={<User />} />
+            </Routes>
+        </>
+    );
 };
 
 export default AuthenicationRoute;
